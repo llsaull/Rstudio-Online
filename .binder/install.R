2fba0054afc00dd832e38f545ea4369bc655cabc
aluno <aluno@gmail.com>
2020-07-24 16:13:50 -0300

    Organizing

--- .binder/install.R
+++ .binder/install.R
@@ -2,3 +2,7 @@ install.packages('rmarkdown')
 install.packages('knitr')
 library(tinytex)
 tinytex::install_tinytex()
+
+system("git config user.email aluno@gmail.com")
+system("git config user.name aluno")
+system("git config credential.helper cache")

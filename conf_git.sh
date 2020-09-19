f44f78c845172700a1059c23cbbdcde1efa12b4b
Saul Leite <llsaull@gmail.com>
2020-09-19 16:45:50 -0300

    new file:   conf_git.sh 	modified:   postBuild

---
+++ conf_git.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo Qual seu login do github
+read login
+echo Qaul seu email
+read email
+git config --global user.email $email
+git config --global user.name $login
+git config --global credential.helper store
+Rscript ~/install.R

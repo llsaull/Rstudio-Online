f01ed56d9d939c15d562734e1e26a4fd495732d9
Saul Leite <llsaull@gmail.com>
2020-10-23 13:11:07 -0300

    keras 6

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 library(tinytex)
 tinytex::install_tinytex()
-library(keras)
-install_keras(method="conda", conda="/srv/conda/condabin/conda")
+#library(keras)
+#install_keras(method="conda", conda="/srv/conda/condabin/conda")
